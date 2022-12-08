cfb24c3a7dde0cf2f59e1d9834c0ba2627ac1bf7
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2022-12-08 11:16:08 -0800

    Bug fix the number one drawing off screen

--- fNum.py
+++ fNum.py
@@ -6,60 +6,93 @@ import math
 
 #TODO Make the line drawing antialiased or something to make to look like a real pencil line
 
-# This is a custom line that will be able to draw a line with each pixel being a different color
-def testLine(draw: ImageDraw, image: Image, size):
-    # Get the start and end points of the base of the one
-    baseStartX = int(size[0]/2) + rd.randint(-2,1)
-    baseStartY = rd.randint(3,10)
-    baseEndX = int(size[0]/2) + rd.randint(-2,1)
-    baseEndY = size[1] - rd.randint(0,10)
-
-    # Get the coordinates for the little head at the top of the one
-    pointY = rd.randint(-1, 3)
-    if pointY == -1:
-        headEndX = baseStartX - rd.randint(1,2)
-    elif pointY == 3:
-        headEndX = baseStartX - rd.randint(2,3)
-    else:
-        headEndX = baseStartX - rd.randint(1,3)
-
-    headEndY = baseStartY + pointY
-
-    # Get the coordinates for the little tail at the end of the one
-    pointY = rd.randint(-3,3)
-    if pointY > 1 or pointY < -1:
-        pointX = rd.randint(2,3)
-    else:
-        pointX = rd.randint(1,3)
-    tailStartX = baseEndX - pointX
-    tailStartY = baseEndY + pointY
-
-    tailEndX = baseEndX + pointX
-    tailEndY =  baseEndY - pointY
-
-
-    # Not super sure but this all just doesn't work
-    # draw the first base line
-    cl.drawLine(draw=draw, coordOne=(baseStartY, baseStartX), coordTwo=(baseEndY, baseEndX))
-
-    # draw the first head line
-
-    # draw the first tail line
-    cl.drawLine(draw=draw, coordOne=(tailStartX, tailStartY), coordTwo=(tailEndX, tailEndY))
-
-def main():
-    # The format and size of the file
+# Custom class to hold the image
+class cImage: 
+    # Format, size and background of file
     FORMAT = 'RGB'
-    SIZE = (15,40)
+    SIZE = (15,30)
     BACKGROUND = (250 + rd.randint(-5,5), 250 + rd.randint(-5,5), 250 + rd.randint(-5,5))
 
-    # create a new image
     image = Image.new(FORMAT, SIZE, BACKGROUND)
-
-    # Draw on the new image
     draw = ImageDraw.Draw(image)
 
-    cl.drawOne(draw,image, image.size)
+    def __init__(self) -> Image:
+        pass
+    
+    def drawLine(self, coordOne, coordTwo):
+        x0 = coordOne[0]
+        y0 = coordOne[1]
+        x1 = coordTwo[0]
+        y1 = coordTwo[1]
+
+        # Set x and y to starting positions
+        y = y0
+        x = x0
+
+        # get difference between start and end points
+        dx = x1 - x0
+        dy = y1 - y0
+
+        # Get distance between points
+        dxy = math.sqrt((dx**2 + dy**2))
+
+        # difference to be added to x and y at each interval
+        dx /= dxy
+        dy /= dxy
+
+        # loop through line and plot points
+        for _ in range(int(dxy)):
+            self.draw.point((round(x),round(y)), (0, 0, 0))
+            x += dx
+            y += dy
+
+    def drawOne(self):
+        # Get the start and end points of the base of the one
+        baseStartX = int(self.image.size[0]/2) + rd.randint(-2,1)
+        baseStartY = rd.randint(3,10)
+        baseEndX = int(self.image.size[0]/2) + rd.randint(-2,1)
+        baseEndY = self.image.size[1] - rd.randint(4,10)
+
+        # Get the coordinates for the little head at the top of the one
+        pointY = rd.randint(-1, 3)
+        if pointY == -1:
+            headEndX = baseStartX - rd.randint(1,2)
+        elif pointY == 3:
+            headEndX = baseStartX - rd.randint(2,3)
+        else:
+            headEndX = baseStartX - rd.randint(1,3)
+
+        headEndY = baseStartY + pointY
+
+        # Get the coordinates for the little tail at the end of the one
+        pointY = rd.randint(-3,3)
+        if pointY > 1 or pointY < -1:
+            pointX = rd.randint(2,3)
+        else:
+            pointX = rd.randint(1,3)
+        tailStartX = baseEndX - pointX
+        tailStartY = baseEndY + pointY
+
+        tailEndX = baseEndX + pointX
+        tailEndY =  baseEndY - pointY
+
+        # Draw the base one line
+        self.drawLine(coordOne=(baseStartX, baseStartY), coordTwo=(baseEndX, baseEndY))
+        # draw the base tail
+        self.drawLine(coordOne=(tailStartX, tailStartY), coordTwo=(tailEndX, tailEndY))
+
+        # draw base head
+        self.drawLine(coordOne=(headEndX, headEndY), coordTwo=(baseStartX, baseStartY))
+    
+    def show(self):
+        self.image.show()
+        
+def main():
+    # create a new image
+    image = cImage()
+
+    # Draw on the new image
+    image.drawOne()
 
     image.show()
 
